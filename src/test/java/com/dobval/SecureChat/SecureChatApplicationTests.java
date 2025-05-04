be46513a0275b8279c66ac533083afa0c5204c32
dobriev <dobromirv@proton.me>
2025-05-04 20:17:49 +0300

    Spring initializr

---
+++ src/test/java/com/dobval/SecureChat/SecureChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobval.SecureChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
