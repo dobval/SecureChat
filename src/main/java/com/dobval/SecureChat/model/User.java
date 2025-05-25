c7c47c2ade53b1e5e8fa24ee97dba72d08b190f3
dobriev <dobromirv@proton.me>
2025-05-25 21:56:14 +0300

    Fixed db not being persistent (issue was the testcontainers config)

--- src/main/java/com/dobval/SecureChat/model/User.java
+++ src/main/java/com/dobval/SecureChat/model/User.java
@@ -1,21 +1,28 @@
 package com.dobval.SecureChat.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name = "users") //Otherwise 'user' is a reserved keyword in Postgre 
+@Table(name = "users") //Otherwise 'user' is a reserved keyword in Postgre
+@NoArgsConstructor // JPA requires a no-arg constructor
+@AllArgsConstructor // handy for testing or manual instantiation
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
-    
+
+    @Column(nullable = false)
     private String password;
 }
